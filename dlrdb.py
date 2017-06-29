8742016b785dd88726bf9cad8a8480e11a06157f
CKAN <CKAN>
2017-06-29 15:39:30 +0200

    added dataPeriod()

--- dlrdb.py
+++ dlrdb.py
@@ -6,6 +6,7 @@ This file contains functions to fetch data from the Domestic Load Research datab
 
 import pandas as pd
 import pyodbc 
+from datetime import datetime
 
 def getData(tablename = None, querystring = 'SELECT * FROM tablename'):
     "This function fetches a specified table from the DLR database and returns it as a pandas dataframe."
@@ -39,3 +40,37 @@ def getData(tablename = None, querystring = 'SELECT * FROM tablename'):
     df = pd.DataFrame(results)
     return df
 
+def dataPeriod(dataframe, startdate = None, enddate = None):
+    "This function selects a subset of a dataframe based on a date range." 
+    "Dates must be formated as 'YYYY-MM-DD'. Days start at 00:00 and end at 23:55"
+    
+    #print profile date info
+    dataStart = dataframe['Datefield'].min()
+    dataEnd = dataframe['Datefield'].max()
+    print('Profile starts on %s. \nProfile ends on %s.' % (dataStart, dataEnd))
+    
+    #prompt for user input if no start and end dates were provided
+    startdate = input('Enter period start date as YYYY-MM-DD\n') if startdate is None else startdate
+    enddate = input('Enter period end date as YYYY-MM-DD\n') if enddate is None else enddate
+    
+    #convert start and end date user input to datetime object
+    if isinstance(startdate, str):
+        startdate = datetime.strptime(startdate + ' 00:00', '%Y-%m-%d %H:%M')
+    if isinstance(enddate, str):
+        enddate = datetime.strptime(enddate + ' 23:55', '%Y-%m-%d %H:%M')
+    
+    #check that input dates fall within the profile period
+    if startdate > enddate :
+        return print('Period start must be before period end.')
+    if datetime.date(startdate) == datetime.date(dataStart): #set start date to data start to avoid time error
+        startdate = dataStart
+    if datetime.date(enddate) == datetime.date(dataEnd): #set end date to data end to avoid time error
+        enddate = dataEnd
+    if (startdate < dataStart) | (startdate > dataEnd):
+        return print('This profile starts on %s and ends on %s. Choose a start date that falls within this period.' % (dataStart, dataEnd))
+    if (enddate < dataStart) | (enddate > dataEnd):
+        return print('This profile starts on %s and ends on %s. Choose an end date that falls within this period.' % (dataStart, dataEnd))
+    
+    #subset dataframe by the specified date range
+    df = dataframe.loc[(dataframe['Datefield'] >= startdate) & (dataframe['Datefield'] <= enddate)]
+    return df
--- fetchdata.py
+++ fetchdata.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from dlrdb import getData
+from dlrdb import getData, dataPeriod
 
 #specify and execute query(ies)
 query1 = 'SELECT * FROM [General_LR4].[dbo].[linktable] WHERE ProfileID = 12005320'
@@ -29,8 +29,15 @@ ORDER BY pt.Datefield, pt.ProfileID'
 
 
 df = getData(querystring = query)
+
+#subset profile on date-time
+april2012 = dataPeriod(df, '2012-04-01', '2012-04-30')
+
 #select all the average current values and sum for hourly data
 AvgA = df.loc[(df['Description']=='A avg'), ['Datefield','Unitsread']]
+
 #AvgA['ZeroA'] =  AvgA.apply(lambda x : 1 if (x['Unitsread'] == 0) else 0, axis = 1) #mark zero readings with 1
 HourlyA = AvgA.resample('60Min', on='Datefield').mean()
 HourlyA['2012-06-01'].plot()
+
+profiles = pd.crosstab(index=april2012['Datefield'], columns=[april2012['RecorderID'], april2012['ProfileID'], april2012['Description']], values = april2012['Unitsread'], aggfunc = sum)
