56c9bbf449f0fd6595d46a2cd21a36c75d2ff50d
CKAN <CKAN>
2017-07-03 19:35:05 +0200

    optimising getProfiles

--- fetchdata.py
+++ fetchdata.py
@@ -11,15 +11,22 @@ import feather
 
 from sqldlr import getData, getProfileID, getProfiles, getSampleProfiles, profilePeriod, getGroups
 
-def saveAllProfiles(mypath):
+def saveAllProfiles(mypath, yearstart, yearend):
     "This function fetches all profile data and saves it to path as a .feather file. It will take several hours to run!"
-    for i in range(1994,2015):
+    for i in range(yearstart, yearend + 1):
         print(i)
         df = getProfiles(i)
         path = mypath + 'p' + str(i) + '.feather'
         print(path)
         feather.write_dataframe(df, path)
-        
+
+def savetables(datadict): 
+    #paths = ['E:\\Domestic Load Research DB\\DBTables\\' + k + '.feather' for k in files]
+    keys = datadict.keys()
+    for k in keys:
+        data = datadict[k].fillna(np.nan, inplace = True) #feather doesn't write None type
+        path = 'E:\\Domestic Load Research DB\\DBTables\\' + k + '.feather'
+        feather.write_dataframe(data, path)
 
 # EXAMPLES
 ## Groups
@@ -49,15 +56,15 @@ HourlyA['2012-06-01'].plot()
 profiles = pd.crosstab(index=april2012['Datefield'], columns=[april2012['RecorderID'], april2012['ProfileID'], april2012['Description']], values = april2012['Unitsread'], aggfunc = sum)
 
 ## Saving and loading
-p1994.to_csv('E:\\Domestic Load Research DB\\Profiles\\p1994.csv', index=False, date_format='%Y-%m-%d %H:%M') #pretty rubbish. CSV can't cope
-load94 = pd.read_csv('E:\\Domestic Load Research DB\\Profiles\\p1994.csv', dtype={'ProfileID':'category', 'RecorderID':'category', 'UoM':'category'}, parse_dates=['Datefield'])
+p1994.to_csv('E:\\Domestic Load Research DB\\DBTables\\Profiles\\p1994.csv', index=False, date_format='%Y-%m-%d %H:%M') #pretty rubbish. CSV can't cope
+load94 = pd.read_csv('E:\\Domestic Load Research DB\\DBTables\\Profiles\\p1994.csv', dtype={'ProfileID':'category', 'RecorderID':'category', 'UoM':'category'}, parse_dates=['Datefield'])
 april1994 = profilePeriod(load94, '1994-04-01', '1994-04-30')
 
 #super fast data storage & retrieval with feather
 #NB NOT FOR LONG TERM STORAGE!!
 sp95 = getSampleProfiles(1995)
 p95 = getProfiles(1995)
-path = 'E:\\Domestic Load Research DB\\Profiles\\p95.feather'
+path = 'E:\\Domestic Load Research DB\\DBTables\\Profiles\\p95.feather'
 feather.write_dataframe(p95, path)
 feather95 = feather.read_dataframe(path)
 
--- sqldlr.py
+++ sqldlr.py
@@ -18,17 +18,16 @@ import pandas as pd
 import pyodbc 
 from datetime import datetime
 
-def getData(tablename = None, querystring = 'SELECT * FROM tablename'):
+def getData(tablename = None, querystring = 'SELECT * FROM tablename', chunksize = 10000):
     "This function fetches a specified table from the DLR database and returns it as a pandas dataframe."
     
     #create connection object
     with open('cnxnstr.txt', 'r') as f: 
         cnxnstr = f.read().replace('\n', '')
     cnxn = pyodbc.connect(cnxnstr)
-    cursor = cnxn.cursor()
-
+    
     #specify and execute query(ies)
-    if querystring == 'SELECT * FROM tablename':
+    if querystring == "SELECT * FROM tablename":
         if tablename is None:
             return print('Specify a valid table from the DLR database')
         elif tablename == 'Profiletable':
@@ -37,28 +36,10 @@ def getData(tablename = None, querystring = 'SELECT * FROM tablename'):
             query = "SELECT * FROM [General_LR4].[dbo].%s" % (tablename)
     else:
         query = querystring
-    cursor.execute(query)
-    
-    while True:
-        rows = cursor.fetchmany(1000)
-        if not rows:
-            break
-        for r in rows:
-            yield r
-    
-    #rows = cursor.fetchall() #fetch all query data 
-    description = cursor.description #get result details
-    colnames = [x[0] for x in description] #table column names
-#    datatypes = [x[1] for x in description] #column data types
-
-    #create dataframe with results
-    results = []
-    for row in rows:
-        results.append(dict(zip(colnames, row)))
-    df = pd.DataFrame(results)
-    
-    cnxn.close()
-
+        
+    #read to dataframe    
+    df = pd.read_sql(query, cnxn)
+        
     return df
 
 def getProfileID(year = None):
