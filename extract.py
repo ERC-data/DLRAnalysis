2e84ad96d0e18d896df0b8c006aa2418fb17de9f
CKAN <CKAN>
2017-08-03 15:28:05 +0200

    esap pc changes

--- extract.py
+++ extract.py
@@ -8,35 +8,41 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import feather
+import extract_support
 
-from sqldlr import getData, getProfileID, getProfiles, getSampleProfiles, profilePeriod, getGroups
+dbTables = ['Answers','Answers_Number','','','','','','','']
+
+
+# save anonymised answer_blob and answer_char tables
+extract_support.anonAns()
+
 
 def saveAllProfiles(mypath, yearstart, yearend):
     "This function fetches all profile data and saves it to path as a .feather file. It will take several hours to run!"
     for i in range(yearstart, yearend + 1):
         print(i)
-        df = getProfiles(i)
+        df = extract_support.getProfiles(i)
         path = mypath + 'p' + str(i) + '.feather'
         print(path)
         feather.write_dataframe(df, path)
 
 # EXAMPLES
 ## Groups
-groups = getGroups() #get all groups
-g2000 = getGroups(2000) #get groups for 2000
+groups = extract_support.getGroups() #get all groups
+g2000 = extract_support.getGroups(2000) #get groups for 2000
 
 ## Profiles
-pids = getProfileID()
-pids2000 = getProfileID(2000)
-sp2000 = getSampleProfiles(2000)
+pids = extract_support.getProfileID()
+pids2000 = extract_support.getProfileID(2000)
+sp2000 = extract_support.getSampleProfiles(2000)
 #p1994 = getProfiles(1994)
 
 # Get profiles by specifying and executing query(ies)
 query = 'SELECT * FROM [General_LR4].[dbo].[linktable] WHERE ProfileID = 12005320'
-df = getData(querystring = query)
+df = extract_support.getData(querystring = query)
 
 #subset profile on date-time
-april2012 = profilePeriod(df, '2012-04-01', '2012-04-30')
+april2012 = extract_support.profilePeriod(df, '2012-04-01', '2012-04-30')
 
 #select all the average current values and sum for hourly data
 AvgA = df.loc[(df['Description']=='A avg'), ['Datefield','Unitsread']]
@@ -52,11 +58,4 @@ p1994.to_csv('E:\\Domestic Load Research DB\\DBTables\\Profiles\\p1994.csv', ind
 load94 = pd.read_csv('E:\\Domestic Load Research DB\\DBTables\\Profiles\\p1994.csv', dtype={'ProfileID':'category', 'RecorderID':'category', 'UoM':'category'}, parse_dates=['Datefield'])
 april1994 = profilePeriod(load94, '1994-04-01', '1994-04-30')
 
-#super fast data storage & retrieval with feather
-#NB NOT FOR LONG TERM STORAGE!!
-sp95 = getSampleProfiles(1995)
-p95 = getProfiles(1995)
-path = 'E:\\Domestic Load Research DB\\DBTables\\Profiles\\p95.feather'
-feather.write_dataframe(p95, path)
-feather95 = feather.read_dataframe(path)
 
