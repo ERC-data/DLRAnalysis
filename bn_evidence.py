c968c0feeb9808a8fa80980b1c60bfccc1a08565
SaintlyVi <wiebke.toussaint@gmail.com>
2017-10-05 14:26:46 +0200

    evidence creation for BN

--- bn_evidence.py
+++ bn_evidence.py
@@ -7,13 +7,21 @@ Created on Sun Jul 23 13:59:37 2017
 
 import pandas as pd
 import numpy as np
-from socios import buildFeatureFrame, checkAnswer, loadTables
+import json
+from socios import buildFeatureFrame
+import os
+from pathlib import Path
+
+src_dir = str(Path(__file__).parents[0])
+dlrdb_dir = str(Path(__file__).parents[1])
+output_dir = os.path.join(dlrdb_dir, 'output')
 
 #List of random variables in Bayesian network
 bn_n = ["monthly_income", "water_access", "roof_material", "wall_material", "cb_size", "floor_area", "geyser_nr"]
 
 def evidence2000(year):
     """
+    This function generates an evidence text file compatible with the python library libpgm for the specified year.
     """
     
     #Define socio-demographic search terms corresponding to BN nodes - valid for surveys from 2000 onwards
@@ -22,7 +30,6 @@ def evidence2000(year):
     #Get data and questions from socio-demographic survey responses
     data = buildFeatureFrame(searchlist, year)
     featureframe = data[0]
-    questions = data[1]
     
     featureframe['geyser_nr'] = featureframe['115'] - featureframe['131'] #geyser_nr = GeyserNumber - GeyserBroken
     featureframe.drop(['115','131'], axis=1, inplace=True) #remove superfluous geyser columns
@@ -42,10 +49,12 @@ def evidence2000(year):
     featureframe.wall_material = featureframe.wall_material.map('{:.0f}'.format, na_action=None)
     featureframe.cb_size = featureframe.cb_size.map('{:.0f}'.format, na_action=None)
     featureframe.geyser_nr = featureframe.geyser_nr.map('{:.0f}'.format, na_action=None)
+    featureframe.AnswerID = featureframe.AnswerID.map('{:.0f}'.format, na_action=None)
     
     featureframe.replace(np.nan, '', regex=True, inplace=True) #easier to remove empty string later than nan, which can behave unpredictably
     featureframe.set_index('AnswerID', inplace=True) #set AnswerID column as index
     
+    #Convert dataframe into a dict formatted for use as evidence in libpgm BN inference
     featuredict = featureframe.to_dict('index') 
     e = []
     for f in featuredict.values(): 
@@ -54,25 +63,13 @@ def evidence2000(year):
             if v is not str(''):
                 d[k] = v
         e.append(d)  
-    
     evidence = dict(zip(featuredict.keys(), e))
     
-    return evidence
-
-#testanswercheck = checkAnswer(915, 'iron')
-
-# NEED TO ASSOCIATE ANSWERID WITH PROFILEID
-
-#mydata = featureFrame(features, 2011)[0]
-#mydata = mydata.drop(['AnswerID','95'], axis=1)
-#cols = ['Hi','Wa','Rm','Wm','Te']
-#mydata.columns = cols
-
-#playing with profiles
-#kW = feather.read_dataframe('E:\\git\\DLR_DB\\profiles\\raw\\2009\\2009-8\\2009-8_kW.feather')
-#kWh = kW.groupby(['UoM','RecorderID','ProfileID']).resample('1H', on='Datefield').sum()
-#kWd = kW.groupby(['UoM','RecorderID','ProfileID']).resample('1D', on='Datefield').sum()
-#kWm = kW.groupby(['UoM','RecorderID','ProfileID']).resample('1M', on='Datefield').sum()
-
-#recorder_ids = kW.RecorderID.cat.categories.tolist()
-
+    #Generate evidence file
+    filename = 'bn_evidence_' + str(year) + '.txt'
+    filepath = os.path.join(output_dir, filename)
+    with open(filepath, 'w') as f:
+        json.dump(evidence, f)
+    print(filename + ' successfully saved to ' + output_dir)
+    
+    return #evidence
